533e59c7020a17dff019c266a374e47d007ad4f4
Adarsh.M <adarshmelath1305@gmail.com>
2026-01-11 17:22:16 +0530

    variables added

--- .p2/Desktop/BrandNewJavaScriptStudy/README.md
+++ .p2/Desktop/BrandNewJavaScriptStudy/README.md
@@ -5,3 +5,9 @@
 -   Single Line Comment(//)
 
 -   Multi Line Comment(/\*\*/)
+
+## Variables
+
+-   A variable is a “named storage” for data.
+
+-   we can declare a variable using var,let,const
---
+++ .p2/Desktop/BrandNewJavaScriptStudy/variables/Variables.js
@@ -0,0 +1,13 @@
+//declaring the variable
+//using var
+
+var name = 'Adarsh';
+console.log(name);
+
+//using let
+let age = 22;
+console.log(age);
+
+//using const
+const PI = 3.14; //can't reassign
+console.log(PI);
