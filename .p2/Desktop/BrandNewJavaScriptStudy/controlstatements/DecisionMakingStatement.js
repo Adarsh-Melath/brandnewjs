2c8f97ee9ed761f3dcaa281b4a5e28e64108b77c
Adarsh.M <adarshmelath1305@gmail.com>
2026-01-11 20:32:47 +0530

    switch statement added

---
+++ .p2/Desktop/BrandNewJavaScriptStudy/controlstatements/DecisionMakingStatement.js
@@ -0,0 +1,22 @@
+//if-else
+let userName = 'Albin';
+if (userName === 'Adarsh') {
+    console.log('Hello ' + userName);
+} else {
+    console.log('Hello Guest');
+}
+
+//switch
+let color = 'Green';
+
+switch (color) {
+    case 'red':
+        console.log('Stop!');
+        break;
+    case 'Green':
+        console.log('Go!');
+        break;
+    default:
+        console.log('Broken Light');
+        break;
+}
