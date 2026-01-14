8b5a6fdcc5aaacf5c97cb60786de810d90a76b3c
Adarsh.M <adarshmelath1305@gmail.com>
2026-01-14 20:16:13 +0530

    the four pillars of oop concept added

---
+++ .p2/Desktop/BrandNewJavaScriptStudy/objectorientedjs/ObjectOrientedJs.js
@@ -0,0 +1,100 @@
+//encapsulation
+
+class MyCar {
+    constructor(brand, color) {
+        (this.brand = brand), (this.color = color);
+    }
+
+    startEngine() {
+        console.log(`${this.brand} engine started!`);
+    }
+}
+
+const car1 = new MyCar('Toyota', 'Red');
+
+car1.startEngine();
+
+//abstarction
+
+class BankAccount {
+    constructor(accountNumber, balance) {
+        this.accountNumber = accountNumber;
+
+        let _balance = balance;
+
+        this.checkBalance = () => {
+            console.log(`Current balance: ${_balance}`);
+        };
+
+        this.deposit = (amount) => {
+            if (amount > 0) {
+                _balance += amount;
+                console.log(
+                    `${amount} deposited successfully! Current balance ${_balance}`
+                );
+            } else {
+                console.log('Deposit amount must be positive!');
+            }
+        };
+
+        this.withdraw = (amount) => {
+            if (amount > 0 && amount <= _balance) {
+                _balance -= amount;
+                console.log(
+                    `${amount} withdrawn successfully! Current balance ${_balance}`
+                );
+            } else {
+                console.log('Invalid withdrawal amount!');
+            }
+        };
+    }
+}
+
+const account = new BankAccount('123456789', 1000);
+
+account.checkBalance();
+account.withdraw(300);
+account.deposit(500);
+
+//inheritance  and polymorpshim
+
+class Animal {
+    constructor(name) {
+        this.name = name;
+    }
+
+    eat() {
+        console.log(`${this.name} is eating`);
+    }
+
+    speaking() {
+        console.log(`${this.name} is making a sound`);
+    }
+}
+
+class Dog extends Animal {
+    constructor(name) {
+        super(name);
+    }
+
+    speaking() {
+        console.log(`${this.name} barks`);
+    }
+}
+
+class Cat extends Animal {
+    constructor(name) {
+        super(name);
+    }
+
+    speaking() {
+        console.log(`${this.name} meows`);
+    }
+}
+const dog = new Dog('Buddy');
+const cat = new Cat('Bunny');
+dog.eat();
+dog.speaking();
+
+cat.eat();
+cat.speaking();
