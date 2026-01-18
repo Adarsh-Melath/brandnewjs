cc55be4950910ae7d97b374f0df5e7d33dd0e02f
Adarsh.M <adarshmelath1305@gmail.com>
2026-01-18 08:08:06 +0530

    static method printLivingSpace added

--- .p2/Desktop/BrandNewJavaScriptStudy/objects/Class.js
+++ .p2/Desktop/BrandNewJavaScriptStudy/objects/Class.js
@@ -4,12 +4,18 @@ class User {
         this.email = email;
         this.age = age;
     }
-
-    display() {
+    display = function () {
         console.log(this.name, this.email, this.age);
-    }
+    };
+
+    static livingSpace = 'earth';
+
+    static printLivingSpace = () => {
+        console.log(this.livingSpace);
+    };
 }
 
 const user1 = new User('Adarsh', 'adarsh@gmail.com', 18);
 
 user1.display();
+User.printLivingSpace();
