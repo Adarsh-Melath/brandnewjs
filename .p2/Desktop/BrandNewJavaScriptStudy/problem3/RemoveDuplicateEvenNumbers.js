8d2cdc325f79a7538ade961cc6f20a7a7ea1c423
Adarsh.M <adarshmelath1305@gmail.com>
2026-01-29 21:17:29 +0530

    Remove duplicate even numbers without inbuilt methods added

---
+++ .p2/Desktop/BrandNewJavaScriptStudy/problem3/RemoveDuplicateEvenNumbers.js
@@ -0,0 +1,12 @@
+//without in built methods
+// const numbers = [2];
+
+// const newNumberArray = [];
+
+// for (let number of numbers) {
+//     if (newNumberArray.includes(number) === false && number % 2 == 0)
+//         newNumberArray.push(number);
+//     else if (number % 2 == 1) newNumberArray.push(number);
+// }
+
+// console.log(newNumberArray);
