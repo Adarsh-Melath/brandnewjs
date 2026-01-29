163aef4fa483cb03d88f7cb4c3a811e2f2b41a30
Adarsh.M <adarshmelath1305@gmail.com>
2026-01-29 22:05:59 +0530

    Sum of even numbers without in built method added

--- .p2/Desktop/BrandNewJavaScriptStudy/problem3/SumOfEvenNumbers.js
+++ .p2/Desktop/BrandNewJavaScriptStudy/problem3/SumOfEvenNumbers.js
@@ -4,8 +4,10 @@ let sum = 0;
 
 //without in built methods
 
-// for (let number of numbers) {
-//     sum += number;
-// }
+for (let number of numbers) {
+    if (number % 2 == 0) {
+        sum += number;
+    }
+}
 
-// console.log(sum);
+console.log(sum);
