2f8bb0db99d6ac3f8840f7a9f369a6688f0c38a8
Adarsh.M <adarshmelath1305@gmail.com>
2026-01-29 22:04:47 +0530

    Sum of even numbers without in built method added

---
+++ .p2/Desktop/BrandNewJavaScriptStudy/problem3/SumOfEvenNumbers.js
@@ -0,0 +1,11 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let sum = 0;
+
+//without in built methods
+
+// for (let number of numbers) {
+//     sum += number;
+// }
+
+// console.log(sum);
