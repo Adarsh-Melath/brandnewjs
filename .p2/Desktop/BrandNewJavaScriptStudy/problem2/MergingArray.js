8eb6075cc810c78c8d97ee787f529f503b64688a
Adarsh.M <adarshmelath1305@gmail.com>
2026-01-29 22:18:43 +0530

    Merge two arrays with inbuilt methods  added

--- .p2/Desktop/BrandNewJavaScriptStudy/problem2/MergingArray.js
+++ .p2/Desktop/BrandNewJavaScriptStudy/problem2/MergingArray.js
@@ -6,14 +6,18 @@ const array1 = [1, 2, 3];
 const array2 = [4, 5, 6];
 const mergedArray = [];
 
-let sizeOfArray1 = array1.length;
-let sizeOfArray2 = array2.length;
+// let sizeOfArray1 = array1.length;
+// let sizeOfArray2 = array2.length;
 
-for (let i = 0; i < sizeOfArray1; i++) {
-    mergedArray[i] = array1[i];
-}
+// for (let i = 0; i < sizeOfArray1; i++) {
+//     mergedArray[i] = array1[i];
+// }
 
-for (let i = 0; i < sizeOfArray2; i++) {
-    mergedArray[sizeOfArray1 + i] = array2[i];
-}
+// for (let i = 0; i < sizeOfArray2; i++) {
+//     mergedArray[sizeOfArray1 + i] = array2[i];
+// }
+
+//with in built support
+
+mergedArray.push(...array1, ...array2);
 console.log(mergedArray);
