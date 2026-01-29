b108019065ea0403b3abc5c9bc80c60dc38139d3
Adarsh.M <adarshmelath1305@gmail.com>
2026-01-29 21:31:01 +0530

    Remove an array element without inbuilt methods added

---
+++ .p2/Desktop/BrandNewJavaScriptStudy/problem3/RemoveAnElementFromArray.js
@@ -0,0 +1,24 @@
+const prompt = require('prompt-sync')();
+
+const numbers = [10, 20, 30, 40, 50];
+const removingNumber = Number(prompt('Enter the number to remove: '));
+
+//without in built method
+// if (numbers.includes(removingNumber)) {
+//     for (let i = 0; i < numbers.length; i++) {
+//         if (removingNumber === numbers[i]) {
+//             removeNumber(i, numbers);
+//         }
+//     }
+// } else {
+//     console.log('Number not found in array');
+// }
+
+// function removeNumber(startIndex, numbers) {
+//     for (let i = startIndex; i < numbers.length; i++) {
+//         numbers[i] = numbers[i + 1];
+//     }
+//     numbers.length--;
+}
+
+console.log(numbers);
