57f6bf61022d1f755bcd739a5eb2b71ea28c01d1
Adarsh.M <adarshmelath1305@gmail.com>
2026-01-31 09:50:45 +0530

    sum of elements in the given array  without  in built methods added

--- .p2/Desktop/BrandNewJavaScriptStudy/problem2/SumOfElementsInGivenArray.js
+++ .p2/Desktop/BrandNewJavaScriptStudy/problem2/SumOfElementsInGivenArray.js
@@ -1,8 +1,14 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-
 //using inbuilt method
 
-const sum = numbers.reduce((prev, current) => prev + current, 0);
-console.log(sum);
+// const sum = numbers.reduce((prev, current) => prev + current, 0);
 
+//without inbuilt method
+let sum = 0;
+
+for (let number of numbers) {
+    sum += number;
+}
+
+console.log(sum);
