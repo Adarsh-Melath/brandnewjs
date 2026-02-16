ce307e04f27a751f1c59926eb5b883de7106f0f2
Adarsh.M <adarshmelath1305@gmail.com>
2026-02-16 17:38:15 +0530

    example for InputStreamReader added

--- .p2/Desktop/javaN/filehandling/streams/characterStream/filewriter/FileWriterExample.java
+++ .p2/Desktop/javaN/filehandling/streams/characterStream/filewriter/FileWriterExample.java
@@ -5,7 +5,13 @@ public class FileWriterExample {
     public static void main(String[] args) {
         try {
             FileWriter fw = new FileWriter("filehandling\\streams\\characterStream\\filewriter\\example.txt", true);
-            fw.write("Hey I am Adarsh");
+            // writing string to a file
+            // fw.write("Hey I am Adarsh");
+
+            // writing an array of characters to a file
+            char[] ch = { 'j', 'a', 'v', 'a' };
+            fw.write(ch);
+
             fw.close();
         } catch (IOException e) {
             System.out.println(e);
